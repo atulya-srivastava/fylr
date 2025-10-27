d6b628a5e203e5af976c511f61f89a95aa9ee696
atulya srivastava <vns.atulyasrivastava@gmail.com>
2025-10-27 18:54:01 +0530

    docs : Update inferSelect comm for better understanding

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -6,26 +6,26 @@ export const files = pgTable("files", {
 
   //basic filer folder info
   name: text("name").notNull(),
-  path: text("path").notNull(),
+  path: text("path").notNull(), //something like documets/home/abc.png
   size: integer("size").notNull(),
   type:text("type").notNull(), //"folder"
 
    //storage info
    fileUrl: text("file_url").notNull(), // url to access file
-   thumbnailUrl: text("thumbnail_url"), // url to access thumbnail
+   thumbnailUrl: text("thumbnail_url"), // url to access thumbnail can be null
 
-   // Ownership
+   // Ownership who is the parent
    userId: text("user_id").notNull(),
    parentId: uuid("parent_id"), //parent folder id null for root items
 
-   // file/folder flags
+   // file/folder flags , later on we can use filters to just show those files which are not in trash, are starred etc
    isFolder: boolean("is_folder").notNull().default(false),
    isStarred: boolean("is_starred").notNull().default(false),
    isTrash: boolean("is_trash").notNull().default(false),
 
    //Timestamps
-   createdAt: timestamp("created_at").notNull().defaultNow().notNull(),
-   updatedAt: timestamp("updated_at").notNull().defaultNow().notNull(),
+   createdAt: timestamp("created_at").defaultNow().notNull(),
+   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })
 
 /*
@@ -46,5 +46,5 @@ export const filesRelations = relations(files, ({one ,many}) => ({
 
 // Type definations 
 
-export const File = typeof files.$inferSelect; //infer the selection which is good for me
+export const File = typeof files.$inferSelect; //infer the selection which is good for me so that i can use it in the editor which understands this type as i am usimg ts
 export const NewFile = typeof files.$inferInsert; //infer the insertion which is good for me
