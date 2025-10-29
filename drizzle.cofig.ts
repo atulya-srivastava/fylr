fa61ddaa2313d09a71b42edfb7a252b4a3f887e8
atulya srivastava <vns.atulyasrivastava@gmail.com>
2025-10-29 22:55:20 +0530

    setup: add a config file for drizzle

---
+++ drizzle.cofig.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+dotenv.config({path: ".env"});
+
+if(!process.env.DATABASE_URL){
+    throw new Error("Database url is not set in the env file");
+}
+
+export default defineConfig({
+schema: './lib/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  migrations: {
+    table : "__drizzle_migration",
+    schema: "public"
+  },
+  verbose: true,
+  strict: true
+});
+
+
+//this is for the drizzle kit the mechanic
+
