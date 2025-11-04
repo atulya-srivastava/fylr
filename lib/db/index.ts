e7f1c3a5a0261ed62531c36edbef6e31c87bf7bd
atulya srivastava <vns.atulyasrivastava@gmail.com>
2025-11-04 19:07:18 +0530

    chore: load environment variables from .env

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,7 +1,13 @@
 import {drizzle} from 'drizzle-orm/neon-http';
 import {neon} from '@neondatabase/serverless';
-
+import * as dotenv from "dotenv"
 import * as schema from "./schema" //import every export from the ./schema and bundle it into the schema object you have just written contains file, File and NewFile
+
+dotenv.config({path:".env"});
+
+if(!process.env.DATABASE_URL) {
+    throw new Error("Missing DATABASE_URL in environment")
+}
 
 const sql = neon(process.env.DATABASE_URL!); //from the env file 
 
