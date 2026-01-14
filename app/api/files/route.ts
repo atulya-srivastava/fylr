15977ab1ff207642e6def94932f02df0735aab2d
atulya srivastava <vns.atulyasrivastava@gmail.com>
2026-01-14 19:18:00 +0530

    fix: correct typo in error message for file fetching

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -51,6 +51,6 @@ export async function GET(request: NextRequest){
         return NextResponse.json(userFiles)
     } catch (error) {
         console.error("error fecthing",error)
-        return NextResponse.json({error: "Failed to fetched files"},{status: 500});
+        return NextResponse.json({error: "Failed to fetch files"},{status: 500});
     }
 }
